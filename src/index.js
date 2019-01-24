7bbc1fe5465100bf89ec39bffadcd87160d4d853
Dina Ghalini <dinaghalini@gmail.com>
2019-01-24 17:12:18 +0300

    try sdk2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import cart from './models/cart';
 import categories from './models/categories';
 import products from './models/products';
 
-export const sdk = {
+export {
     categories,
     cart,
     products
